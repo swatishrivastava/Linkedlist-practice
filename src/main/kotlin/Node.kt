20fdc68e54d2382b30fe947b77336b20cd33876f
Swati <swati.shrivastava@nagarro.com>
2022-02-17 11:38:54 +0400

    added node class

---
+++ src/main/kotlin/Node.kt
@@ -0,0 +1,9 @@
+data class Node<T : Any>(var value: T, var next: Node<T>) {
+    override fun toString(): String {
+        return if (next != null) {
+            "$value -> ${next.toString()}"
+        } else {
+            "$value"
+        }
+    }
+}
